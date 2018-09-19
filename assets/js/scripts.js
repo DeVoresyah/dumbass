d28f9321ffc65f74a150ea0331641cb3d9d2c2dc
DeVoresyah ArEst <de.voresyah@gmail.com>
2018-09-20 00:32:49 +0700

    additional changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -98,41 +98,8 @@ $(function() {
 			columnWidth: 1
 		  }
 		});
-
-		// filter items on button click
-		$('.filter-button-group').on( 'click', 'button', function() {
-		  var filterValue = $(this).attr('data-filter');
-		  $grid.isotope({ filter: filterValue });
-		});
-		
-		//for menu active class
-		$('.portfolio-button button').on('click', function(event) {
-			$(this).siblings('.active').removeClass('active');
-			$(this).addClass('active');
-			event.preventDefault();
-		});
 	});
 	 
-	/* --------------------------- */
-	/*  6. testimon slider JS
-	/* --------------------------- */  
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-			}
-		}
-	});
 	
 	 
 	
